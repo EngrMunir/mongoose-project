b72e194753a1dd7703fa2f42971eb13a3c4123f7
munir <smsirajulmonir@gmail.com>
2024-11-22 18:27:06 +0600

    data post get complete

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
-import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
-const app:Application = express()
-const port = 3000
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/modules/student/student.route';
+const app: Application = express();
+const port = 3000;
 
 // parsers
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req:Request, res:Response) => {
-  const a=20;
-  res.send(a)
-})
+// api/v1/students/create-student
+
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
+  const a = 20;
+  res.send(a);
+};
+
+app.get('/', getAController);
 
 export default app;
